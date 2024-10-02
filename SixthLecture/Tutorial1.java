6aa443081ada9781710391fdda032c545230b0c5
priya <priyaaashu4@gmail.com>
2024-10-02 22:56:21 +0530

    adding functions files

---
+++ SixthLecture/Tutorial1.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Tutorial1 {
+    public static void main(String[] args) {
+       int ans= sum2();
+       System.out.println(ans);
+    }
+    static int  sum2(){
+        Scanner sc =new Scanner(System.in);
+        System.out.print("enter the num1: ");
+        int num1=sc.nextInt();
+        System.out.print("enter the num2: ");
+        int num2=sc.nextInt();
+        int sum = num1+ num2;
+        return sum;
+       
+    }
+
+    static void sum(){
+        Scanner sc =new Scanner(System.in);
+        System.out.println("enter the num1");
+        int num1=sc.nextInt();
+        System.out.println("enter the num21");
+        int num2=sc.nextInt();
+        int sum = num1+ num2;
+        System.out.println(sum);
+
+    }
+}
